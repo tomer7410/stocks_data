62ca83c8136bf5c15f98ac2e267157fbc4bec095
tomer7410 <rooom784@gmail.com>
2023-09-12 16:32:19 +0300

    read_from_cache

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -2,7 +2,7 @@ import { Inject, Injectable } from '@nestjs/common';
 import { RedisCache, RedisClient } from './interfaces/redis.interface';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { StockEntity } from 'src/stocks/entities/stockDto';
-import { EXPERATION_TIME, JSON_FILE_DIR, LIST_KEY, SHOULD_READ_FROM_FILE } from 'src/utils/const';
+import { EXPERATION_KEY, EXPERATION_TIME, EXPERATION_VALUE, JSON_FILE_DIR, LIST_KEY, SHOULD_READ_FROM_CACHE } from 'src/utils/const';
 import { readFile } from 'fs';
 
 @Injectable()
@@ -18,46 +18,71 @@ export class RedisService {
         this.readStocksDataFromFile().then((stocks:StockEntity[])=>{
             this.writeDataToCache(LIST_KEY,stocks)
             // flag to read from file every 3 hours
-            this.reNewFileReadingExperationTime()
+            this.reNewFileReadingExperationTime(EXPERATION_KEY,EXPERATION_VALUE,EXPERATION_TIME)
+            this.setFlagtoReadFromChace(SHOULD_READ_FROM_CACHE,"true")
             
         })
+    }
+    setFlagtoReadFromChace(key: string, value: string) {
+        this.cacheService.set(key,value).catch(err=>console.log('cannot read rom redis'));
+    }
+    async getReadFromFileFlag(flag: string){
+        return await this.cacheService.get(flag)
     }
     readStocksDataFromFile():Promise< StockEntity[]>{
         return new Promise((resolve,reject)=>{
             readFile(process.cwd()+JSON_FILE_DIR,async(err,data)=>{
                 if(err) resolve([]);
-                resolve(JSON.parse(data.toString()) as StockEntity[]) 
+                resolve(JSON.parse(data.toString()) as StockEntity[]);
             })
         })
         
     }
-    reNewFileReadingExperationTime(){
+    reNewFileReadingExperationTime(key:string, value: string, experationTime: number){
         // renew the experation time
-        this.redisClient.set(SHOULD_READ_FROM_FILE, true, 'EX',EXPERATION_TIME,((err,data)=>{
-            console.log(data);
+        this.cacheService.set(key,value,experationTime)
+        // this.redisClient.set(SHOULD_READ_FROM_FILE, true, 'EX',EXPERATION_TIME,((err,data)=>{
+        //     console.log(data);
             
-        }));
+        // }));
     }
     writeDataToCache(key: string,data:StockEntity[]){
-        this.redisClient.lpush(key, JSON.stringify(key), function(err, data) {
+        this.redisClient.lpush(key, JSON.stringify(data), function(err, data) {
             if (err) throw new Error('cannot push to redis');
             console.log('data write to redis'); 
         });
     }
+    readDataFromCache(key: string){
+        return new Promise((resolve,reject)=>{
+            this.redisClient.lrange(key, 0, 0, function(err, res){
+                if(err) resolve([])
+                else resolve(JSON.parse(res.toString()))
+            })
+        })
+    }
     async handleFileReading(){
-        return new Promise((resolve, reject)=>{
-            this.redisClient.ttl('shouldRead',(async(err, timetoLive) => {
-                if(!err){
-                    let stocksFromFile = null
-                    if(timetoLive<= 0){
-                       // renew experation time
-                        this.reNewFileReadingExperationTime()
-                        stocksFromFile = await this.readStocksDataFromFile()
-                        resolve(stocksFromFile as StockEntity[])
+        return new Promise(async(resolve, reject)=>{
+            const shouldReadfromCache = await this.getReadFromFileFlag(SHOULD_READ_FROM_CACHE)
+            if(shouldReadfromCache === "true"){
+                const data = await this.readDataFromCache(LIST_KEY)
+                resolve(data)
+                this.setFlagtoReadFromChace(SHOULD_READ_FROM_CACHE,"false")
+            }
+            else{
+                this.redisClient.ttl(EXPERATION_KEY,(async(err, timetoLive) => {
+                    if(!err){
+                        let stocksFromFile = null
+                        if(timetoLive<= 0){
+                           // renew experation time
+                           this.reNewFileReadingExperationTime(EXPERATION_KEY,EXPERATION_VALUE,EXPERATION_TIME)
+                            stocksFromFile = await this.readStocksDataFromFile()
+                            resolve(stocksFromFile as StockEntity[])
+                        }
+                        resolve([] as StockEntity[])
                     }
-                    resolve([] as StockEntity[])
-                }
-              }))
+                  }))
+            }
+            
             })
 
     }
--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,5 +1,6 @@
 export const EXPERATION_TIME =  3 * 60 * 60
-export const SHOULD_READ_FROM_FILE = 'shouldReadFromFile'
+export const EXPERATION_KEY = 'experationKey'
+export const EXPERATION_VALUE = 'experationValue'
 export const LIST_KEY = 'stocks'
 export const SHOULD_READ_FROM_CACHE = 'shouldReadFromCache'
 export const JSON_FILE_DIR = '/src/stocksData.json'
